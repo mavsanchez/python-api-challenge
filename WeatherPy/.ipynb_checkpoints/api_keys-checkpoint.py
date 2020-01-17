4c2d3e01e6c7caaf148cbaeb143d9b1a3d1c5d68
Maverick Sanchez <maverickssanchez@gmail.com>
2020-01-17 02:31:10 -0800

    Updating homework submission

--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "bd483eda68e85d7c3bbdef5fc3ee30b9"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDxBnIi0LWnkZmuoVP3XbtwvO2e9qX9Yd4"
